639db2ce8cb484a68ba95d4c24afb5241dea24de
Tu Nuevo Nombre <174087797+GTWENTERPRISES@users.noreply.github.com>
2025-07-22 01:27:38 -0500

    asdasd

--- nuevo/urls.py
+++ nuevo/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from new.views import ProductViewSet
+from django.conf import settings
+from django.conf.urls.static import static
 
 router = routers.DefaultRouter()
 router.register(r'products', ProductViewSet)
@@ -26,5 +28,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
 ]
-#
-#
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
